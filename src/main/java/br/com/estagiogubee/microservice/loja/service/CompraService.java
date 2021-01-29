fe3cad071eae048a91e01c11314f33352dbc0b53
MucaBordini <mucabordini@gmail.com>
2021-01-29 11:36:08 -0300

    Service descovery Eureka

--- src/main/java/br/com/estagiogubee/microservice/loja/service/CompraService.java
+++ src/main/java/br/com/estagiogubee/microservice/loja/service/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.estagiogubee.microservice.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,15 +11,17 @@ import br.com.estagiogubee.microservice.loja.controller.dto.InfoFornecedorDTO;
 
 @Service
 public class CompraService {
+	
+	@Autowired
+	private RestTemplate client;
 
 	public void realizaCompra(CompraDTO compra) {
 
-		RestTemplate client = new RestTemplate();
 		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange(
-				"http://localhost:8081/info/" + compra.getEndereco().getEstado(), HttpMethod.GET, null,
+				"http://fornecedor/info/"+compra.getEndereco().getEstado(), HttpMethod.GET, null,
 				InfoFornecedorDTO.class);
 		
-		System.out.println(exchange.getBody().getEndereco());
+		//System.out.println(exchange.getBody().getEndereco());
 
 	}
 
