83737844a586e13c384ab99b9cf00016a2370d6b
MucaBordini <mucabordini@gmail.com>
2021-02-01 09:12:50 -0300

    importando produtos e pedidos

--- src/main/java/br/com/estagiogubee/microservice/loja/controller/dto/EnderecoDTO.java
+++ src/main/java/br/com/estagiogubee/microservice/loja/controller/dto/EnderecoDTO.java
@@ -31,5 +31,12 @@ public class EnderecoDTO {
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
+
+	@Override
+	public String toString() {
+		return "EnderecoDTO [rua=" + rua + ", numero=" + numero + ", estado=" + estado + "]";
+	}
+	
+	
 	
 }
--- src/main/java/br/com/estagiogubee/microservice/loja/service/CompraService.java
+++ src/main/java/br/com/estagiogubee/microservice/loja/service/CompraService.java
@@ -1,28 +1,34 @@
 package br.com.estagiogubee.microservice.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import br.com.estagiogubee.microservice.loja.client.FornecedorClient;
 import br.com.estagiogubee.microservice.loja.controller.dto.CompraDTO;
 import br.com.estagiogubee.microservice.loja.controller.dto.InfoFornecedorDTO;
+import br.com.estagiogubee.microservice.loja.controller.dto.InfoPedidoDTO;
+import br.com.estagiogubee.microservice.loja.model.Compra;
 
 @Service
 public class CompraService {
 	
 	@Autowired
-	private RestTemplate client;
-
-	public void realizaCompra(CompraDTO compra) {
-
-		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange(
-				"http://fornecedor/info/"+compra.getEndereco().getEstado(), HttpMethod.GET, null,
-				InfoFornecedorDTO.class);
+	private FornecedorClient fornecedorClient;
+	
+	public Compra realizaCompra(CompraDTO compra) {
 		
-		//System.out.println(exchange.getBody().getEndereco());
-
+		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+		
+		InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
+		
+		Compra compraSalva = new Compra();
+		compraSalva.setPedidoId(pedido.getId());
+		compraSalva.setTempoDePreparo(pedido.getTempoDePreparo());
+		compraSalva.setEnderecoDestino(compra.getEndereco().toString());
+		
+		return compraSalva;
+		
+		
 	}
 
 }
